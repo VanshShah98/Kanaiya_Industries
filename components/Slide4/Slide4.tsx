68c5b778f138ba0cdf033e7d5b3d64952b59cf29
VanshShah98 <vanshshah0908@gmail.com>
2024-05-28 09:54:40 +0530

    Bug Fix

--- components/Slide4/Slide4.tsx
+++ components/Slide4/Slide4.tsx
@@ -11,12 +11,12 @@ export default function Slide4() {
           </div>
           <div className='lg:pl-20 pt-6'>
             <Typography className='text-white text-center lg:text-left text-xl italic'>
-              'Esteemed Partners, our Management Team extends a gracious invitation to partake in a symbiotic venture, where cutting-edge expertise converges to deliver unparalleled solutions. By synergizing our technological acumen, we aspire to engineer bespoke excellence that aligns seamlessly with your requisites. Engage with us, and let our collective prowess sculpt the pinnacle of technical innovation, propelling your endeavors to unparalleled heights.'
+              {"Esteemed Partners, our Management Team extends a gracious invitation to partake in a symbiotic venture, where cutting-edge expertise converges to deliver unparalleled solutions. By synergizing our technological acumen, we aspire to engineer bespoke excellence that aligns seamlessly with your requisites. Engage with us, and let our collective prowess sculpt the pinnacle of technical innovation, propelling your endeavors to unparalleled heights."}
             </Typography>
           </div>
           <div className='pb-4'>
             <Typography className='flex flex-col text-white text-3xl lg:text-5xl font-medium font-serif pl-8 lg:pl-10 pt-14 pt-16 pb-16 '>Esteemed Customers</Typography>
-            <InfiniteSlider/>
+            <InfiniteSlider />
           </div>
         </div>
       </section>
--- components/Slide4/infiniteloop.tsx
+++ components/Slide4/infiniteloop.tsx
@@ -1,17 +1,17 @@
 import Image from 'next/image';
 
 const LOGOS = [
-  <Image src="/LOGO/BhartRasayan.svg" width={300} height={300} alt="Bharat Rasayan" />,
-  <Image src="/LOGO/SRF.svg" width={300} height={300} alt="SRF" />,
-  <Image src="/LOGO/GI.svg" width={300} height={300} alt="GI" />,
-  <Image src="/LOGO/Godrej.svg" width={300} height={300} alt="Godrej" />,
-  <Image src="/LOGO/MFL.svg" width={200} height={300} alt="MFL" />,
-  <Image src="/LOGO/NACL.svg" width={300} height={300} alt="NACL" />,
-  <Image src="/LOGO/PI.svg" width={300} height={300} alt="PI" />,
-  <Image src="/LOGO/Tagros.svg" width={300} height={300} alt="Tagros" />,
-  <Image src="/LOGO/Thermax.svg" width={300} height={300} alt="Thermax" />,
-  <Image src="/LOGO/Thirumalai.svg" width={300} height={300} alt="Thirumalai" />,
-  <Image src="/LOGO/UPL.svg" width={300} height={300} alt="UPL" />,
+  <Image src="/LOGO/BhartRasayan.svg" width={300} height={300} alt="Bharat Rasayan" key="bharat-rasayan" />,
+  <Image src="/LOGO/SRF.svg" width={300} height={300} alt="SRF" key="srf" />,
+  <Image src="/LOGO/GI.svg" width={300} height={300} alt="GI" key="gi" />,
+  <Image src="/LOGO/Godrej.svg" width={300} height={300} alt="Godrej" key="godrej" />,
+  <Image src="/LOGO/MFL.svg" width={200} height={300} alt="MFL" key="mfl" />,
+  <Image src="/LOGO/NACL.svg" width={300} height={300} alt="NACL" key="nacl" />,
+  <Image src="/LOGO/PI.svg" width={300} height={300} alt="PI" key="pi" />,
+  <Image src="/LOGO/Tagros.svg" width={300} height={300} alt="Tagros" key="tagros" />,
+  <Image src="/LOGO/Thermax.svg" width={300} height={300} alt="Thermax" key="thermax" />,
+  <Image src="/LOGO/Thirumalai.svg" width={300} height={300} alt="Thirumalai" key="thirumalai" />,
+  <Image src="/LOGO/UPL.svg" width={300} height={300} alt="UPL" key="upl" />,
 ];
 
 export const InfiniteSlider = () => {
@@ -19,15 +19,12 @@ export const InfiniteSlider = () => {
     <div className="relative z-1 m-auto w-5/6 lg:w-full overflow-hidden bg-slate-200 before:absolute before:left-0 before:top-0 before:z-[2] before:h-full before:w-[100px] before:bg-[linear-gradient(to_right,white_0%,rgba(255,255,255,0)_100%)] before:content-[''] after:absolute after:right-0 after:top-0 after:z-[2] after:h-full after:w-[100px] after:-scale-x-100 after:bg-[linear-gradient(to_right,white_0%,rgba(255,255,255,0)_100%)] after:content-['']">
       <div className="lg:h-32 animate-infinite-slider flex w-[calc(250px*10)]">
         {LOGOS.map((logo, index) => (
-          <div
-            className="slide flex w-[125px] items-center justify-center"
-            key={index}
-          >
+          <div className="slide flex w-[125px] items-center justify-center" key={`logo-${index}`}>
             {logo}
           </div>
         ))}
         {LOGOS.map((logo, index) => (
-          <div className="slide flex w-[125px] items-center justify-center" key={index}>
+          <div className="slide flex w-[125px] items-center justify-center" key={`logo-duplicate-${index}`}>
             {logo}
           </div>
         ))}
